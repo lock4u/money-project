83e861049f4b5d9a6d0e9dbc8f323348e68feadd
zhuo.li <zhuo.li@ximalaya.com>
2018-11-26 13:53:28 +0800

    add sql tx support

--- money-basic/src/main/java/org/money/sales/basic/data/MysqlSupporter.java
+++ money-basic/src/main/java/org/money/sales/basic/data/MysqlSupporter.java
@@ -14,8 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Optional;
-
-import static java.util.Optional.empty;
+import java.util.function.Function;
 
 /**
  * Helper and wrapper class for JDBC repository services.
@@ -26,6 +25,16 @@ import static java.util.Optional.empty;
 public abstract class MysqlSupporter {
 
     protected final AsyncSQLClient mysql;
+
+
+    private Function<ResultSet, Optional<JsonObject>> single = resultSet -> {
+        List<JsonObject> rows = resultSet.getRows();
+        if (rows == null || rows.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.ofNullable(rows.get(0));
+        }
+    };
 
 
     @SuppressWarnings("unchecked")
@@ -41,44 +50,12 @@ public abstract class MysqlSupporter {
     }
 
 
-    protected void single(String sql, JsonArray args, Handler<AsyncResult<Optional<JsonObject>>> handler) {
-
-        getConnection(conn -> {
-            if (conn.failed()) {
-                handler.handle(Future.failedFuture(conn.cause()));
-            } else {
-                SQLConnection sc = conn.result();
-                log.debug("@execute command [query],sql [{}] =======>", sql);
-                sc.queryWithParams(sql, args, q -> {
-                    if (q.failed()) {
-                        close(sc, close -> {
-                            if (close.failed()) {
-                                handler.handle(Future.failedFuture(close.cause()));
-                            } else {
-                                handler.handle(Future.failedFuture(q.cause()));
-                            }
-                        });
-                    } else {
-                        ResultSet rs = q.result();
-                        List<JsonObject> rows = rs.getRows();
-                        if (rows == null || rows.isEmpty()) {
-                            handler.handle(Future.succeededFuture(empty()));
-                        } else {
-                            handler.handle(Future.succeededFuture(Optional.of(rows.get(0))));
-                        }
-                    }
-                });
-            }
-        });
+    protected Future<Optional<JsonObject>> single(String sql, JsonArray args) {
+        Future<Optional<JsonObject>> f = Future.future();
+        mysql.queryWithParams(sql, args, q -> f.handle(q.map(single)));
+        return f;
     }
 
-
-    private void getConnection(Handler<AsyncResult<SQLConnection>> handler) {
-        mysql.getConnection(conn -> {
-            log.debug("@getConnection =======> {}", conn);
-            handler.handle(conn);
-        });
-    }
 
     private void close(SQLConnection conn, Handler<AsyncResult<Void>> handler) {
         log.debug("@SqlConnection[{}] closing =======>", conn);
@@ -88,39 +65,86 @@ public abstract class MysqlSupporter {
         });
     }
 
-    public  <T> void tx(Handler<AsyncResult<T>> handler, Command<T> c) {
 
-        Future<SQLConnection> fc = Future.future();
+    private Future<Void> close(SQLConnection conn, boolean rollback) {
+        Future f = Future.future();
 
-        mysql.getConnection(conn -> {
-            if (conn.failed()) {
-                fc.fail(conn.cause());
-            } else {
-                SQLConnection sc = conn.result();
-                sc.setAutoCommit(false, auto -> {
-                    if (auto.failed()) {
-                        fc.fail(auto.cause());
-                    } else {
-                        Future<T> exec = c.exec(sc);
-                        exec.setHandler(cc -> {
-                            if (cc.failed()) {
-                                sc.rollback(rollback -> {
-                                    if (rollback.failed()) {
-                                        handler.handle(Future.failedFuture(rollback.cause()));
-                                    } else {
-                                        sc.close(close -> {
-                                            if (close.failed()) {
-                                                handler.handle(Future.failedFuture(close.cause()));
-                                            } else {
-                                                handler.handle(Future.failedFuture(cc.cause()));
-                                            }
-                                        });
-                                    }
-                                });
-                            } else {
-                                handler.handle(cc);
+        if (rollback) {
+
+            conn.rollback(new Handler<AsyncResult<Void>>() {
+                @Override
+                public void handle(AsyncResult<Void> rollback) {
+                    if (rollback.failed()) {
+                        conn.close(new Handler<AsyncResult<Void>>() {
+                            @Override
+                            public void handle(AsyncResult<Void> close) {
+
                             }
                         });
+                    }
+                }
+            });
+        } else {
+
+        }
+
+        conn.close(new Handler<AsyncResult<Void>>() {
+            @Override
+            public void handle(AsyncResult<Void> close) {
+                if (close.failed()) {
+                    conn.rollback()
+                    f.fail(close.cause());
+                } else {
+                    f.complete();
+                }
+            }
+        });
+
+    }
+
+
+    public <T> void tx(Command<T> c, Handler<AsyncResult<T>> completer) {
+        mysql.getConnection(getConnection -> {
+            if (getConnection.failed()) {
+                completer.handle(Future.failedFuture(getConnection.cause()));
+            } else {
+                SQLConnection sc = getConnection.result();
+                sc.setAutoCommit(false, autoCommit -> {
+                    if (autoCommit.failed()) {
+                        completer.handle(Future.failedFuture(autoCommit.cause()));
+                    } else {
+
+                        Future co =
+                                c.run(sc);
+//                                .setHandler(new Handler<AsyncResult<T>>() {
+//                                    @Override
+//                                    public void handle(AsyncResult<T> exec) {
+//                                        if (exec.failed()) {
+//                                            sc.rollback(new Handler<AsyncResult<Void>>() {
+//                                                @Override
+//                                                public void handle(AsyncResult<Void> rollback) {
+//                                                    if (rollback.failed()) {
+//                                                        completer.handle(Future.failedFuture(rollback.cause()));
+//                                                    } else {
+//
+//                                                    }
+//                                                }
+//                                            });
+//
+//                                        } else {
+//                                            sc.close(new Handler<AsyncResult<Void>>() {
+//                                                @Override
+//                                                public void handle(AsyncResult<Void> close) {
+//                                                    if (close.failed()) {
+//                                                        completer.handle(Future.failedFuture(close.cause()));
+//                                                    } else {
+//                                                        completer.handle(exec);
+//                                                    }
+//                                                }
+//                                            });
+//                                        }
+//                                    }
+//                                });
                     }
                 });
             }
@@ -129,9 +153,31 @@ public abstract class MysqlSupporter {
     }
 
 
-    public static interface Command<T> {
+    public interface Command<T> {
 
-        Future<T> exec(SQLConnection sql);
+        Future<T> run(final SQLConnection connection);
     }
 
+
+    public <T> Handler<AsyncResult<T>> invoke(SQLConnection connection, Future<T> future) {
+        return result -> {
+            if (result.failed()) {
+                connection.rollback(rollback -> {
+                    if (rollback.failed()) {
+                        future.fail(rollback.cause());
+                    } else {
+                        connection.close(close -> {
+                            if (close.failed()) {
+                                future.fail(close.cause());
+                            } else {
+                                future.fail(result.cause());
+                            }
+                        });
+                    }
+                });
+            } else {
+                future.complete(result.result());
+            }
+        };
+    }
 }
